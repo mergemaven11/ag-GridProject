72c9a634e793ad10e52e4b7acdeea94ab27f0ce7
tscott2023 <38087336+Tobias2023@users.noreply.github.com>
2020-02-24 16:19:08 -0500

    Add styling, getSelRows functionality, add api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AgGridAngular } from "ag-grid-angular";
+
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'the-app';
+  @ViewChild("agGrid", { static: false }) agGrid: AgGridAngular;
+  title = 'app';
 
   columnDefs = [
-    { headerName: "Make", field: "make" },
-    { },
-    {}
-  ]
+    { headerName: 'Make', field: 'make', sortable: true, filter: true, checkboxSelection: true },
+    { headerName: 'Model', field: 'model', sortable: true, filter: true },
+    { headerName: 'Price', field: 'price', sortable: true, filter: true }
+  ];
+
+  rowData: any;
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.rowData = this.http.get('https://api.myjson.com/bins/ly7d1'); 
+   }
+
+  getSelRows() {
+    const selectedNodes = this.agGrid.api.getSelectedNodes();
+    const selectedData = selectedNodes.map(node => node.data);
+    const selectedDataStringPresentation = selectedData
+      .map( node => node.make + ' ' + node.model)
+      .join(", ");
+    alert(`Selected Nodes: ${selectedDataStringPresentation}`);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms"; // <-- NgModel lives here
 
-
-// Http client 
-
-import { HttpClientModule } from "@angular/common/http"
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AgGridModule } from 'ag-grid-angular';
+import { HttpClientModule } from "@angular/common/http"
+import 'ag-grid-enterprise';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
     HttpClientModule,
     AgGridModule.withComponents([])
   ],
